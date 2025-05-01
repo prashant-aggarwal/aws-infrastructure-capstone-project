c69e19c611191a192d92311ac8e1126dcb3d5b04
prashant-aggarwal <prashant.aggarwal05@gmail.com>
2025-05-01 15:29:12 +0000

    Added terraform files

---
+++ 3_create_website_terraform/modules/security-groups/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "The name of the current project."
+  type        = string
+  default     = "AWS Infrastructure Capstone Project"
+}
+
+variable "vpc_id" {
+  type = string
+}
