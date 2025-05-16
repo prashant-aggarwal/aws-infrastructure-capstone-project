4a9e20e4ce8e5bb3f11844bac5b9ad033c902c44
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-16 19:53:06 +0530

    Updated main.tf

--- 3_create_website_terraform/modules/subnets/main.tf
+++ 3_create_website_terraform/modules/subnets/main.tf
@@ -1,2 +1,22 @@
+resource "aws_subnet" "subnet-a" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_a_cidr
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = var.add_public_ip
 
+  tags = {
+    Name = "${var.project}-vpc-subnet-a"
+  }
+}
+
+resource "aws_subnet" "subnet-b" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_b_cidr
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = var.add_public_ip
+
+  tags = {
+    Name = "${var.project}-vpc-subnet-b"
+  }
+}
 
