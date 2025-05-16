c50b5945a7204071e0dcb1b53dccae2bad864f64
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-16 19:55:39 +0530

    Removed subnets from variables.tf

--- 3_create_website_terraform/modules/vpc/variables.tf
+++ 3_create_website_terraform/modules/vpc/variables.tf
@@ -4,22 +4,8 @@ variable "project" {
   default     = "AWS Infrastructure Capstone Project"
 }
 
-variable "region" {
-  type    = string
-  default = "us-east-1"
-}
-
 variable "vpc_cidr" {
   type    = string
   default = "10.10.0.0/16"
 }
 
-variable "subnet_a_cidr" {
-  type    = string
-  default = "10.10.1.0/24"
-}
-
-variable "subnet_b_cidr" {
-  type    = string
-  default = "10.10.2.0/24"
-}
