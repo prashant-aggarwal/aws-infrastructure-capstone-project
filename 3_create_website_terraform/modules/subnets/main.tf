ecff046bfca4ad9694c1e7676e8c6966f4a782b8
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-17 17:54:16 +0530

    Added two more subnets for private and public combination

--- 3_create_website_terraform/modules/subnets/main.tf
+++ 3_create_website_terraform/modules/subnets/main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "subnet-a" {
   vpc_id                  = var.vpc_id
   cidr_block              = var.subnet_a_cidr
   availability_zone       = "${var.region}a"
-  map_public_ip_on_launch = var.add_public_ip
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.project}-vpc-subnet-a"
@@ -13,10 +13,32 @@ resource "aws_subnet" "subnet-b" {
   vpc_id                  = var.vpc_id
   cidr_block              = var.subnet_b_cidr
   availability_zone       = "${var.region}b"
-  map_public_ip_on_launch = var.add_public_ip
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.project}-vpc-subnet-b"
+  }
+}
+
+resource "aws_subnet" "subnet-c" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_c_cidr
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-vpc-subnet-c"
+  }
+}
+
+resource "aws_subnet" "subnet-d" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_d_cidr
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-vpc-subnet-d"
   }
 }
 
