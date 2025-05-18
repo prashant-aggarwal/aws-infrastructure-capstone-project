475b3e4d394ec8a9931a14318a406428a7f50262
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-18 15:08:36 +0530

    Renamed variables for consistency

--- 3_create_website_terraform/modules/security-groups/output.tf
+++ 3_create_website_terraform/modules/security-groups/output.tf
@@ -1,7 +1,7 @@
-output "allow_http_lb" {
+output "sg_allow_http_lb_id" {
   value = aws_security_group.allow-http-lb.id
 }
 
-output "allow_http_lb_ec2" {
+output "sg_allow_http_lb_ec2_id" {
   value = aws_security_group.allow-http-lb-ec2.id
 }
