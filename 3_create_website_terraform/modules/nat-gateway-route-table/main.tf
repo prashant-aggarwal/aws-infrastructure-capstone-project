bf8c3d729362c83060502e726c9096b239a6e6c9
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-18 14:14:12 +0530

    Create two route tables for each private subnet

--- 3_create_website_terraform/modules/nat-gateway-route-table/main.tf
+++ 3_create_website_terraform/modules/nat-gateway-route-table/main.tf
@@ -1,27 +1,35 @@
-resource "aws_route_table" "public-rt" {
+resource "aws_route_table" "private-rt-subnet-c" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block     = var.vpc_cidr
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = var.nat_gateway_subnet_a_id
   }
 
+  tags = {
+    Name = "${var.project}-private-rt-subnet-c"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_subnet_c_association" {
+  subnet_id      = var.subnet_c_id
+  route_table_id = aws_route_table.private-rt-subnet-c.id
+}
+
+resource "aws_route_table" "private-rt-subnet-d" {
+  vpc_id = var.vpc_id
+
   route {
-    cidr_block     = var.vpc_cidr
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = var.nat_gateway_subnet_b_id
   }
 
   tags = {
-    Name = "${var.project}-public-rt"
+    Name = "${var.project}-private-rt-subnet-d"
   }
 }
 
-resource "aws_route_table_association" "public_rt_association_a" {
-  subnet_id      = var.subnet_c_id
-  route_table_id = aws_route_table.public-rt.id
-}
-
-resource "aws_route_table_association" "public_rt_association_b" {
+resource "aws_route_table_association" "private_rt_subnet_c_association_d" {
   subnet_id      = var.subnet_d_id
-  route_table_id = aws_route_table.public-rt.id
+  route_table_id = aws_route_table.private-rt-subnet-d.id
 }
