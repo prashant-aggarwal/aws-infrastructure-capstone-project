b82ac848662d53b935c7e146ed7ff7a61f4c2c0d
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-18 11:32:27 +0530

    Calling Elastic IP, NAT GW, NAT RT modules.

--- 3_create_website_terraform/website/main.tf
+++ 3_create_website_terraform/website/main.tf
@@ -15,6 +15,8 @@ module "subnets" {
   project       = var.project
   subnet_a_cidr = var.subnet_a_cidr
   subnet_b_cidr = var.subnet_b_cidr
+  subnet_c_cidr = var.subnet_c_cidr
+  subnet_d_cidr = var.subnet_d_cidr
   add_public_ip = var.add_public_ip
 
   # Passed from VPC Module
@@ -30,6 +32,47 @@ module "security-groups" {
 
   # Passed from VPC Module
   vpc_id = module.vpc.vpc_id
+}
+
+# Create Elastic IPs for NAT Gateways
+module "elastic-ips" {
+  source = "../modules/elastic-ips"
+
+  # Variables
+  project = var.project
+}
+
+# Create NAT Gateways
+module "nat-gateway" {
+  source = "../modules/nat-gateway"
+
+  # Variables
+  project = var.project
+
+  # Passed from Elastic IPs module
+  nat_gateway_eip_1_id = module.elastic-ips.nat_gateway_eip_1_id
+  nat_gateway_eip_2_id = module.elastic-ips.nat_gateway_eip_2_id
+
+  # Passed from Subnets Module
+  subnet_a_id = module.subnets.subnet_a_id
+  subnet_b_id = module.subnets.subnet_b_id
+}
+
+# Create Private Route Table for NAT Gateways
+module "nat-gateway-route-table" {
+  source = "../modules/nat-gateway-route-table"
+
+  # Variables
+  project = var.project
+  vpc_cidr = var.vpc_cidr
+
+  # Passed from Subnets Module
+  subnet_c_id = module.subnets.subnet_c_id
+  subnet_d_id = module.subnets.subnet_d_id
+
+  # Passed from NAT Gateway module
+  nat_gateway_subnet_a_id = module.nat-gateway.nat_gateway_subnet_a_id
+  nat_gateway_subnet_b_id = module.nat-gateway.nat_gateway_subnet_b_id
 }
 
 # Create the Load Balancer
@@ -65,8 +108,8 @@ module "autoscaling-group" {
   add_public_ip      = var.add_public_ip
 
   # Passed from Subnets Module
-  subnet_a_id = module.subnets.subnet_a_id
-  subnet_b_id = module.subnets.subnet_b_id
+  subnet_c_id = module.subnets.subnet_c_id
+  subnet_d_id = module.subnets.subnet_d_id
 
   # Passed from Sec Groups Module
   allow_http_id = module.security-groups.allow_http_id
