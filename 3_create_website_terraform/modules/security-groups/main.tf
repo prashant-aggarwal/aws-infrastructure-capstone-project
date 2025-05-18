ff5bf706ae6dc8f31c2a707cb7665c59ef580d46
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-05-18 15:03:25 +0530

    Segregated SGs for ALB and EC2 instances

--- 3_create_website_terraform/modules/security-groups/main.tf
+++ 3_create_website_terraform/modules/security-groups/main.tf
@@ -1,41 +1,47 @@
-resource "aws_security_group" "allow-http" {
-  name        = "${var.project}-allow-http"
-  description = "Enable HTTP Access"
+resource "aws_security_group" "allow-http-lb" {
+  name        = "${var.project}-allow-http-lb"
+  description = "Allow HTTP Traffic for ALB"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP Access"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  
+  tags = {
+    Name = "${var.project}-allow-http-lb"
   }
 }
 
-resource "aws_security_group" "allow-ssh" {
-  name        = "${var.project}-allow-ssh"
-  description = "Enable SSH Access"
+resource "aws_vpc_security_group_ingress_rule" "allow-http-lb-ingress" {
+  security_group_id = aws_security_group.allow-http-lb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow-http-lb-egress" {
+  security_group_id = aws_security_group.allow-http-lb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_security_group" "allow-http-lb-ec2" {
+  name        = "${var.project}-allow-http-lb-ec2"
+  description = "Allow HTTP Traffic from ALB to EC2 instances"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH Access"
+  
+  tags = {
+    Name = "${var.project}-allow-http-lb-ec2"
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "allow-http-lb-ec2-ingress" {
+  security_group_id = aws_security_group.allow-http-lb-ec2.id
+  referenced_security_group_id = aws_security_group.allow-http-lb.id
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow-http-lb-ec2-egress" {
+  security_group_id = aws_security_group.allow-http-lb-ec2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
